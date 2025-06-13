064a3468963a4145c9e0c36f62e830b877051daf
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-13 22:30:41 +0530

    nodejs2

--- 2-how-node-works/event-loop.js
+++ 2-how-node-works/event-loop.js
@@ -8,6 +8,14 @@ setImmediate(() => console.log("Immediaten1 finished"))
 
 fs.readFile("text-file.txt", () => {
     console.log("I/O finished");
+
+
+setTimerout(() => console.log("Timer 2 finished"), 0);
+setTimerout(() => console.log("Timer 3 finished"), 3000);
+setImmediate(() => console.log("Immediate 2 finished"))
+
+
+
 });
 
 console.log("Hello from the top level code");
