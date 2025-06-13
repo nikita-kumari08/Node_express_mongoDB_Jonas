aa040cef7eaa3c7f7174856a6ca5a8a5a852c2be
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-13 23:36:12 +0530

    nodejs2

---
+++ 2-how-node-works/module.js
@@ -0,0 +1,11 @@
+//console.log(Arguments);
+// console.log(required("module").wrapper);
+
+
+//module.exports
+const C = required("./test-module-1");
+const calc1 = new C();
+console.log(calc1.add(2, 5));
+
+
+// exports 
