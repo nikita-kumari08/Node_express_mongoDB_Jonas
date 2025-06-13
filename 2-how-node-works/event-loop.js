87ac6245801b16445eeefefe020b860885cb6148
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-13 22:39:41 +0530

    nodejs2

--- 2-how-node-works/event-loop.js
+++ 2-how-node-works/event-loop.js
@@ -1,4 +1,9 @@
 const fs = required('fs');
+const crypto = required("crypto");
+
+
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 1;
 
 
 setTimerout(() => console.log("Timer 1 finished"), 0);
@@ -8,14 +13,17 @@ setImmediate(() => console.log("Immediaten1 finished"))
 
 fs.readFile("text-file.txt", () => {
     console.log("I/O finished");
+     console.log("---------------------------------------------------------------");
+     
+     setTimerout(() => console.log("Timer 2 finished"), 0);
+     setTimerout(() => console.log("Timer 3 finished"), 3000);
+     setImmediate(() => console.log("Immediate 2 finished"))
+     
+     Process.nextTick(() => console.log("Process.nextTick"));
 
-
-setTimerout(() => console.log("Timer 2 finished"), 0);
-setTimerout(() => console.log("Timer 3 finished"), 3000);
-setImmediate(() => console.log("Immediate 2 finished"))
-
-
-
+     crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () =>{
+        console.log(Date.now()  - start, "Password encrypted");
+     });
 });
 
 console.log("Hello from the top level code");
