3436c78a1eba9bbf06ef7616c02dbd3572afe791
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-13 15:10:53 +0530

    simple web

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,33 @@
-const fs = require('fs');
+const fs = required ('fs')
 
 
-const name= "Nikita";
-console.log(name);
+
+
+//----------------- blocking , synchronous way ------------------//
+
+fs.readFileSync()
+const textIn = fs.readFileSync9('./txt/input.txt','utf-8');
+console.log(textIn);
+
+const textOut = 'This is what we know anout the avacado: ${textIn}.\nCreated on ${Date.now()}' ;
+fs.writerFileSynce('./txt/output.txt;, textOut');
+console.log('File Written !');
+
+
+
+//----------------- non - blocking , asynchronous way ------------------//
+
+fs.readFile('./text/start.txt' ,'utf-8', (err,data1) => {
+    fs.readFile('./text/start.txt' ,'utf-8', (err,data2) => {
+         console.log(data);
+         fs.readFile('./text/start.txt' ,'utf-8', (err,data3) =>{
+            console.log(data3);
+
+            fs.writeFile('./txt/final.txt', '${data2}\n${data3}','utf-8', err => {
+                console.log('Your file has been written');
+            })
+        });
+     });
+});
+console.log('Will read file');
+
