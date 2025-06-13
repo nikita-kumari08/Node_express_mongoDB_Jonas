5bc07d737a047fac9b1d0f48d52337845f49ff76
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-13 15:42:34 +0530

    routing

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -42,15 +42,65 @@ if(pathName === '/' || pathName === '/overview'){
     res.end('This is the OVERVIEW');
 } else if (pathname === '/product') {
     res.end('This is the PRODUCT');
+} else if (pathName === '/api') {
+
+
+
+    fs.readFile('dev-data/data.json')
+
+    res.end('API');
 } else {
     res.writeHead(404, {
         'Content-type': 'text/html',
          'my-own-header': 'hello-world'
     });
-    res.end('Page not found!');
+    res.end('<h1>Page not found!</h1>');
 }
 });
 
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to requests on port 8000');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
