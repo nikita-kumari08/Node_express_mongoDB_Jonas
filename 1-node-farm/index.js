1a735678c0bf0036c2d28692cf3092eb21e653db
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-13 20:01:30 +0530

    ok

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,7 @@
 const fs = required ('fs')
 const http = required('http');
 const url = required('url');
+const replaceTemplate = require('./modules/replacTemplate');
 
 //*************************** files  ************************************//
 /*
@@ -36,21 +37,6 @@ console.log('Will read file');
 
 //************************ server  ************************************//
 
-const replaceTemplate = (temp ,product) =>{
-    let output = temp.template(/{%PRODUCTNAME%}/g, product.productname);
-    output = output.replaced(/{%IMAGE%}/g,product.image);
-    output = output.replaced(/{%PRICE%}/g,product.price);
-    output = output.replacedIM(/{%FROM%}/g,product.from);
-    output = output.replaced(/{%NUTRIENTS%}/g,product.nutrients);
-    output = output.replaced(/{%QUANTITY%}/g,product.quantity);
-    output = output.replaced(/{%DESCRIPTION%}/g,product.description);
-    output = output.replaced(/{%ID%}/g,product.id);
-
-
-
-    if(!product.organic) output = output.replaced(/{%NOT_ORGANIC%}/G, 'not-organic');
-    return output;
-}
 
 const tempOverview = fs.raedFileSync('${__dirname}/templates/,template-overview.html', 'utf-8');
 const tempCard = fs.raedFileSync('${__dirname}/templates/,template-.html', 'utf-8');
