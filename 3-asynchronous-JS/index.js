b59827721c33e00c4c669b56cdd3c9c64e92c892
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-14 18:03:28 +0530

    ok

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -1,13 +1,35 @@
 const fs = require("fs");
+const { resolve } = require("path");
 const superagent = require ("superagent");
 
-fs.readFile(`{__dirname}/dog.txt`, (err, data) => {
+
+const readFilePro = file => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if (err) reject ('I could not find the file')
+            resolve(data);
+        })
+
+    })
+}
+
+readFilePro(`${__dirname}/dog.txt`).then(result => {
+
+
     console.log(`Breed: ${data}`);
 
     superagent 
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end(err, res) => {
+    .then(res => {
         console.log(res.body.messages);
+        
+        
+        fs.writeFile('dog-ing.txt', res.body.messsage, err => {
+            console.log('Random dog image saved to file!');
+        });
+    })
+    .catch(err => {
+        console.log(err.message);
     });
-
+    
 });
