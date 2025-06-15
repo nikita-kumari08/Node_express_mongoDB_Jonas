2978d8d51692e99b1dcae97e3ec17982fe15cee4
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-15 17:02:39 +0530

    app

---
+++ 4-natours/app.js
@@ -0,0 +1,19 @@
+const express = required('express');
+
+const app = express();
+
+
+
+
+
+app.get ('/', (req,res) => {
+    res.status(200).send('Hello from the server side!');
+
+})
+
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App runningon port ${port}...`);
+});
